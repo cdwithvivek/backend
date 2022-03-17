53500288b213361651767ecb2f1eb904eb9a0982
cdwithvivek <vivekp93btechcse2023@kccitm.edu.in>
2022-03-17 14:12:22 +0530

    function invoke will run if require in other module

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,7 +1,11 @@
 const names = require('./4-name');
 const fun = require('./5-util');
+const data = require('./6-alt-module');
+require('./7-mind-grenade');
 // console.log(names)
 // console.log(fun)
+// console.log(data)
+
 fun.sayHi('amit kumar');
 fun.sayHi(names.vivek);
 fun.sayHi(names.vishal);
